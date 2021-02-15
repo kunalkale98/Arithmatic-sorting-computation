44fd5a4555f2d678956580e775b1b9fe0870193c
kunalkale98 <kunal,k2898@gmail.com>
2021-02-15 15:22:44 +0530

    Added: Sort Result in Desc

--- arithmaticSorting.sh
+++ arithmaticSorting.sh
@@ -10,7 +10,7 @@ echo $op1
 op2=$(($a*$b+$c))
 echo $op2
 
-op3=`expr $c+$a/$b | bc -l`
+op3=$(($c+$a/$b))
 echo $op3
 
 op4=$(($a%$b+$c))
@@ -30,3 +30,19 @@ arr[((2))]="${compute[3]}"
 arr[((3))]="${compute[4]}"
 
 echo "Values in array: "${arr[*]}
+
+len=${#arr[*]}
+for ((i=0;i<$len;i++))
+do
+	for ((j=$(($i+1));j<$len;j++))
+	do
+		if [ ${arr[i]} -lt ${arr[j]} ]
+			then
+				temp=${arr[i]}
+				arr[((i))]=${arr[j]}
+				arr[((j))]=$temp
+		fi
+	done
+done
+
+echo "Descending Order: "${arr[*]}
